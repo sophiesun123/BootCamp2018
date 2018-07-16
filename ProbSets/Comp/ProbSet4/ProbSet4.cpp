1dbbf990d9070e8a97dc6bd65b6fcb0190858222
Sophie Sun <sophiemsun@gmail.com>
2018-07-16 17:44:21 -0500

    probset4

--- ProbSets/Comp/ProbSet4/ProbSet4.cpp
+++ ProbSets/Comp/ProbSet4/ProbSet4.cpp
@@ -7,16 +7,16 @@ S(100, N) = 1 / (0.04 + (1 - 0.04) / 100) = 2.5
 */
 
 // EXERCISE 2
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-// 	string name;
-// 	cout << "Enter name: ";
-// 	cin >> name;
-// 	cout << "Hello, " << name << " from Sophie!" << endl;
-// 	return 0;
-// }
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cout << "Enter name: ";
+	cin >> name;
+	cout << "Hello, " << name << " from Sophie!" << endl;
+	return 0;
+}
 
 // EXERCISE 3 WHY DOESN'T THIS WORK AS;DOIJAO;EIJFAO;WJEF;AOWJEFO;AJWEO;FJAO;WEJFO;AJWEF
 #include <iostream>
@@ -44,59 +44,59 @@ int main()
 }
 
 // EXERCISE 4
-// #include <iostream>
-// using namespace std;
-// static long num_steps = 1000000; double step;
-// int main()
-// {
-// 	int i;
-// 	double x, pi, sum = 0.0;
-// 	step = 1.0 / (double) num_steps;
-// 	for (i = 0; i < num_steps; i++)
-// 	{
-// 		x = (i + 0.5) * step;
-// 		sum = sum + 4.0 / (1.0 + x * x);
-// 	}
-// 	pi = step * sum;
-// 	cout << pi << endl;
-// 	return 0;
-// }
+#include <iostream>
+using namespace std;
+static long num_steps = 1000000; double step;
+int main()
+{
+	int i;
+	double x, pi, sum = 0.0;
+	step = 1.0 / (double) num_steps;
+	for (i = 0; i < num_steps; i++)
+	{
+		x = (i + 0.5) * step;
+		sum = sum + 4.0 / (1.0 + x * x);
+	}
+	pi = step * sum;
+	cout << pi << endl;
+	return 0;
+}
 
 // EXERCISE 5
-// #include <iostream>
-// #include <random>
-// using namespace std;
-// // function that determines whether or not the dart lands in the circle
-// bool circ(double x, double y)
-// {
-// 	double incirc;
-// 	incirc = x * x + y * y;
-// 	return incirc <= 1;
-// }
-// int main()
-// {
-// 	// create random generator
-// 	unsigned seed = 3;
-// 	default_random_engine randnum(seed);
-// 	uniform_real_distribution<double> distr(0.0, 1.0);
+#include <iostream>
+#include <random>
+using namespace std;
+// function that determines whether or not the dart lands in the circle
+bool circ(double x, double y)
+{
+	double incirc;
+	incirc = x * x + y * y;
+	return incirc <= 1;
+}
+int main()
+{
+	// create random generator
+	unsigned seed = 3;
+	default_random_engine randnum(seed);
+	uniform_real_distribution<double> distr(0.0, 1.0);
 
-// 	// random numbers
-// 	double x, y;
-// 	// number of darts in circle and total number of darts thrown
-// 	int in = 0;
-// 	int tot = 10000;
-// 	// create experiment
-// 	for (int i = 1; i < tot; i++)
-// 	{
-// 		x = distr(randnum);
-// 		y = distr(randnum);
-// 		if (circ(x, y))
-// 		{
-// 			in += 1;
-// 		}
-// 	}
-// 	// estimate Pi according to the Monte Carlo method
-// 	double pi = 4. * in / tot;
-// 	cout << pi << endl;
-// 	return 0;
-// }
+	// random numbers
+	double x, y;
+	// number of darts in circle and total number of darts thrown
+	int in = 0;
+	int tot = 10000;
+	// create experiment
+	for (int i = 1; i < tot; i++)
+	{
+		x = distr(randnum);
+		y = distr(randnum);
+		if (circ(x, y))
+		{
+			in += 1;
+		}
+	}
+	// estimate Pi according to the Monte Carlo method
+	double pi = 4. * in / tot;
+	cout << pi << endl;
+	return 0;
+}
